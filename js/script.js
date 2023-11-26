64f4d9fd581a3ba592cf481961f3f3313fae2980
erlanmarinho <erlan.marinho1125@gmail.com>
2023-11-26 16:20:07 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, notANumber } from "./utils.js"
+
+
+// variaveis - variebles
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => AlertError.close()
+inputWeight.oninput = () => AlertError.close()
+
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotAnumber = notANumber(weight) || notANumber(height)
+
+  if (weightOrHeightIsNotAnumber) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) { 
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+
+
+
